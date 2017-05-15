f00b8036c1d19416a45cb2fc7301ac140e3de56a
Yassine Chaouche <yacinechaouche@yahoo.com>
2017-05-15 11:18:56 +0100

    Update basictest.py

--- basictest.py
+++ basictest.py
@@ -1,7 +1,7 @@
 from selenium.webdriver import Firefox
 from selenium.webdriver.support.ui import WebDriverWait
 import sys
-#ff = Firefox(capabilities={"marionette":True})
+
 
 def click_on(id):
     WebDriverWait(ff,10).until(lambda x:ff.find_element_by_id(id))
@@ -21,11 +21,14 @@ URL="https://csgo.steamanalyst.com/list"
 URL2="https://dota2.steamanalyst.com/list"
 OUTFILE="/tmp/table.xls"
 out=open(OUTFILE,"w")
-
+out.write("<table>")
 ff = Firefox()
+#ff = Firefox(capabilities={"marionette":True})
 ff.get(URL)
 for x in xrange(10):
     raw_input("Press Enter to continue")
     get_price_list()
     ff.find_element_by_id("pricelist_next").find_elements_by_tag_name("a")[0].click()
-out.close
+out.write("</table>")
+out.close()
+ff.close()
